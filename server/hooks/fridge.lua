c85aa771b774f8710fc8c22da9e74ef1545cdc6a
Maximus7474 <the.maximus.7474@gmail.com>
2026-05-09 19:11:44 +0200

    doc(hooks/fridge): added explanations to config values

--- server/hooks/fridge.lua
+++ server/hooks/fridge.lua
@@ -1,7 +1,13 @@
 Hooks = Hooks or {}
 
+-- Config values
+
+-- the string identifier to determine a stash is a fridge
 local fridgePattern = "fridge"
+-- 2 means that the time left until it degrades is twice as long
 local durabilityIncrease = 2
+
+-- Business end
 
 local cache = {} --[[ @as table<string, { degrade: number } | false> ]]
 
@@ -23,8 +29,6 @@ Hooks.Fridge = function ()
 	---@param payload SwapItemsPayload
 	---@return boolean
 	function (payload)
-		lib.print.info('swap item data', payload)
-
 		-- boolean values
 		local toFridge = type(payload.toInventory) == "string" and type(payload.toInventory:match(fridgePattern)) == "string"
 		local fromFridge = type(payload.fromInventory) == "string" and type(payload.fromInventory:match(fridgePattern)) == "string"
@@ -37,15 +41,12 @@ Hooks.Fridge = function ()
 		local item = payload.fromSlot
 		local degradeable = hasDegrade(item.name)
 
-		lib.print.info({ item = item.name, degradeable = degradeable, toFridge = toFridge, fromFridge = fromFridge })
-
 		if not degradeable or item.metadata.durability == 0 then return true end
 
 		local currentTime = os.time()
 		local secondsLeft = item.metadata.durability - currentTime
 		if secondsLeft <= 0 then return true end
 
-		lib.print.info('Item is subject to degrade update')
 
 		local inventory = payload.toInventory
 		local slotId = type(payload.toSlot) == "number" and payload.toSlot or payload.toSlot.slot --[[ @as number ]]
@@ -65,13 +66,10 @@ Hooks.Fridge = function ()
 		))
 
 		Citizen.SetTimeout(100, function ()
-			lib.print.info('cur meta for item', item.metadata)
 			local newMeta = item.metadata
 
 			newMeta.degrade = newDegrade
 			newMeta.durability = newDurability
-
-			lib.print.info('new meta for item', newMeta)
 
 			exports.ox_inventory:SetMetadata(
 				inventory, slotId, newMeta
