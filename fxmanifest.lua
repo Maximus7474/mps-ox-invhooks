8217c7234b9cb7e82915c623dd30b9a3b35d47a1
Maximus7474 <the.maximus.7474@gmail.com>
2026-05-09 19:49:13 +0200

    chore(manifest): bump version v1.0.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ server_only "yes"
 description "ox inventory hooks"
 repository 'https://github.com/Maximus7474/mps-ox-invhooks'
 
-version "0.0.0"
+version "1.0.0"
 
 server_scripts {
 	"@ox_lib/init.lua",
